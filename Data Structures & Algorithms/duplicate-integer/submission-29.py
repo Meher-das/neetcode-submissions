3335e850dc9d0fa6dc7bdd1271312aa1c4306ab5
Meher das <93863134+Meher-das@users.noreply.github.com>
2026-06-03 07:39:20 +0530

    Add: duplicate-integer - submission-29

---
+++ Data Structures & Algorithms/duplicate-integer/submission-29.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        memorySet = set()
+        dir(memorySet)
+        for num in nums:
+            if num in memorySet:
+                return True
+            memorySet.add(num)
+        return False
